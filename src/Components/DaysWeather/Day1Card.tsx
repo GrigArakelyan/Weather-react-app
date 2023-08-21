96a1ec8cc2ebc87f12e0b2982f6c9bbe0f059fb7
GrigArakelyan <araqelyan461@gmail.com>
2023-08-21 16:53:01 +0400

    fix project

--- src/Components/DaysWeather/Day1Card.tsx
+++ src/Components/DaysWeather/Day1Card.tsx
@@ -11,26 +11,26 @@ type PropDays = {
 const DayCard:FC<PropDays> = ({day, daysName}) => {
 
 
-   return (
+    return ( 
       <div className="day_card">
       <div className="day">
-         {daysName[new Date(day[0].dt_txt).getDay()]}
+         {daysName[new Date(day[0]?.dt_txt).getDay()]}
       </div>
       <div className="temperature">
          <p className="temp">
-            {Math.floor(day[0].main.temp_max - 273.15)}
+            {Math.floor(day[0]?.main.temp_max - 273.15)}
          </p>
          <p className="temp_o">o</p>
          <p className="temp_C">C</p>
       </div>
       <div className="min_temperature">
-         {Math.floor(day[0].main.temp_min - 273.15)}
+         {Math.floor(day[0]?.main.temp_min - 273.15)}
       </div>
       <div className="img_div">
          {day[0] && <img className="svg" src={`https://openweathermap.org/img/wn/${day[0].weather[0].icon}@2x.png`}/>}
       </div>
       <div className="rain_percent">
-         {`${day[0].main.humidity} %`}
+         {`${day[0]?.main.humidity} %`}
       </div>
    </div>
    )
